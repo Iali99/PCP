df705ae48a8ecd87ff74a9b314e6cf19c04ffbe0
Irfan Ali <cs16btech11019@iith.ac.in>
2019-02-14 19:18:26 +0530

    final commit for assignment 2

---
+++ Assignment2/CS16BTECH11019-FilterAlgo.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <pthread.h>
+#include <unistd.h>
+#include <chrono>
+#include <random>
+#include <stdio.h>
+
+#include "FilterLock.cpp"
+
+using namespace std;
+
+struct ThreadArgs {
+  int id;
+};
+
+void* threadFunction(void * args);
+FilterLock filter;
+int n,k,l1,l2;
+double t1,t2,avg;
+FILE * output;
+std::chrono::time_point<std::chrono::system_clock> start;
+
+int main() {
+
+  cin >> n;
+  cin >> k;
+  cin >> l1;
+  cin >> l2;
+
+  output = fopen("Filter-Output.txt","w");
+  std::default_random_engine generator1;
+  std::default_random_engine generator2;
+  std::exponential_distribution<double> distribution(l1);
+  t1 = distribution(generator1);
+  // printf("t1 = %lf\n",t1 );
+  std::exponential_distribution<double> distribution2(l2);
+  t2 = distribution2(generator2);
+  // printf("t2 = %lf\n",t2 );
+  avg = 0;
+  filter.setDefaults(n);
+  start = std::chrono::system_clock::now();
+  pthread_t *threads = (pthread_t *)malloc(sizeof(pthread_t)*n);
+  for(int i =0;i<n;i++){
+    struct ThreadArgs *args = (struct ThreadArgs *) malloc(sizeof(struct ThreadArgs));
+    args->id = i;
+    pthread_create(&threads[i],NULL,threadFunction,(void *)args);
+  }
+  for(int i =0;i<n;i++) {
+    pthread_join(threads[i],NULL);
+  }
+  avg /= n*k;
+  // printf("%lf\n",avg );
+  return 0;
+}
+
+void * threadFunction(void * args) {
+  int id = ((struct ThreadArgs *) args) -> id ;
+  double time;
+  for(int i =0;i<k;i++) {
+    auto current1 = std::chrono::system_clock::now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(current1 - start).count();
+    fprintf(output,"%dth CS Request at %lf seconds by thread %d\n",i,time,id );
+    filter.lock(id);
+    auto current2 = std::chrono::system_clock::now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(current2 - start).count();
+
+    time = std::chrono::duration_cast<std::chrono::microseconds>(current2 - current1).count();
+    avg += time;
+
+    auto current4 = std::chrono::system_clock::now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(current4 - start).count();
+
+    fprintf(output,"%dth CS Entry at %lf seconds by thread %d\n",i,time,id );
+    usleep(t1);
+    auto current3 = std::chrono::system_clock::now();
+    time = std::chrono::duration_cast<std::chrono::microseconds>(current3 - start).count();
+    fprintf(output,"%dth CS Exit at %lf seconds by thread %d\n",i,time,id );
+    filter.unlock(id);
+
+    usleep(t2);
+  }
+}
---
+++ Assignment2/README.txt
@@ -0,0 +1,18 @@
+FILES DESCRIPTION :
+BakeryLock.cpp - Implementation of Bakery Lock
+CS16BTECH11019-BakeryAlgo.cpp - This file contains the use of Bakery Lock
+
+FilterLock.cpp - Implementation of Filter Lock
+CS16BTECH11019-FilterAlgo.cpp - This file contains the use of Filter Lock
+
+TO COMPILE :
+Bakery Algorithm : "g++ -std=c++11 -pthread CS16BTECH11019-BakeryAlgo.cpp -o Bakery"
+Filter Algorithm : "g++ -std=c++11 -pthread CS16BTECH11019-FilterAlgo.cpp -o Filter"
+
+TO EXECUTE :
+Bakery Algorithm : "./Bakery < inp-params.txt"
+Filter Algorithm : "./Filter < inp-params.txt"
+
+OUTPUT :
+The log file of Bakery Algorithm is "Bakery-Output.txt"
+The log file of Filter Algorithm is "Filter-Output.txt"
